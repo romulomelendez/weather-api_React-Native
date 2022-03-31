2e5d24b276b6af8fd8ee952e33cb081c324e927f
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2022-03-31 03:03:42 -0300

    inital commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
-export default function App() {
+const App = () => {
+
+  const getWeather = () => {
+
+    let key = ''
+    fetch(`${key}`)
+      .then(response => {
+
+        console.log(response.weather)
+
+      })
+      .catch ( error => console.log(error) )
+
+  }
+
   return (
+
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+
       <StatusBar style="auto" />
+      <Text>Open up App.tsx to start working on your app!</Text>
+
     </View>
-  );
+
+  )
+
 }
 
 const styles = StyleSheet.create({
+
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+
+})
+
+export default App
