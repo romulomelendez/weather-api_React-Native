8013d80f46e422430936013e788ae02932c0d99c
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2022-03-31 21:39:02 -0300

    cleaning the app.tsx file

--- App.tsx
+++ App.tsx
@@ -1,32 +1,27 @@
-import { useState } from 'react'
+//import { useState } from 'react'
 import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
+import { StyleSheet } from 'react-native'
 
 const App: React.FC = () => {
   
-  const [ weatherCondition, setWeatherCondition ] = useState('')
+  // const [ weatherCondition, setWeatherCondition ] = useState({})
 
-  const getWeather = () => {
+  // const getWeather = () => {
 
-    let key = '92f3ea930ce06c45e916044d9f6bcd3e'
-    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=${key}`)
-      .then( response => response.json() )
-      .then( data => console.log(data.weather[0].id) )
-      .catch( error => console.log(error) )
+  //   let key = '92f3ea930ce06c45e916044d9f6bcd3e'
+  //   fetch(`https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=${key}`)
+  //     .then( response => response.json() )
+  //     .then( data => setWeatherCondition(data.weather[0]) )
+  //     .catch( error => console.log(error) )
 
-  }
+  // }
 
   return (
 
     <View style={styles.container}>
 
       <StatusBar style="auto" />
-      <TouchableOpacity style={ styles.button } onPress={ getWeather }>
-
-
-        <Text style={ styles.text }>PRESS HERE</Text>
-
-      </TouchableOpacity>
+      <Weather />
 
     </View>
 
@@ -43,17 +38,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   
-  },
-
-  button: {
-
-    padding: 10,
-    borderRadius: 14,
-    backgroundColor: '#cecece'
-
-  },
-
-  text: { fontSize: 25 }
+  }
 
 })
 
