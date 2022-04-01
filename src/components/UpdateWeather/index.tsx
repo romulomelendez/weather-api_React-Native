4b80084fdaef1d455ec96d87a8432bfa68bc5e4e
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2022-03-31 23:19:25 -0300

    creating a UpdateWeather component

---
+++ src/components/UpdateWeather/index.tsx
@@ -0,0 +1,21 @@
+import { View, Text, TouchableOpacity } from 'react-native'
+
+const UpdateWeather: React.FC = () => {
+
+    return (
+    
+        <View>
+
+            <TouchableOpacity>
+
+                <Text>ícone de atualizar</Text>
+
+            </TouchableOpacity>
+
+        </View>
+
+    )
+
+}
+
+export default UpdateWeather
