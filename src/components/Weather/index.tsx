5ea722988bb99a7b67276dc7f8382ff31c1dab99
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2022-04-01 15:10:14 -0300

    creating an obj to store the datas from weather api

--- src/components/Weather/index.tsx
+++ src/components/Weather/index.tsx
@@ -1,12 +1,36 @@
+import { useState } from 'react'
 import { View, Text } from 'react-native'
 
 const Weather: React.FC = () => {
+
+    interface WeatherProps {
+
+        id: number,
+        main: string,
+        description: string,
+        icon: string,
+		temperature: number,
+        feels_like: number,
+        temp_max: number,
+        temp_min:number,
+        pressure: number,
+        humidity: number
+
+    }
+
+    const [ temperature, setTemperature  ] = useState<WeatherProps>()
+    const [ control, setControl ] = useState(0)
 
     return (
 
         <View >
 
-            <Text>Weather component</Text>
+            {
+            
+                control !== 0 ? <Text>{ temperature?.main }</Text>
+                : <Text>Não foi feita uma consulta ainda { temperature }</Text>
+                
+            }
 
         </View>
 
