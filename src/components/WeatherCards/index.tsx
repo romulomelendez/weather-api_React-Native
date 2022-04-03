cab9339589dbab136d5d8c72a0528853fa698144
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2022-04-03 19:49:02 -0300

    deleting unusefull elements from react-native

--- src/components/WeatherCards/index.tsx
+++ src/components/WeatherCards/index.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useContext } from 'react'
-import { StyleSheet, View, Text, TouchableOpacity } from 'react-native'
+import { StyleSheet, View, Text } from 'react-native'
 
 import { WEATHER_API_URL, WEATHER_API_URL_PART2, WEATHER_API_KEY } from '@env'
 
